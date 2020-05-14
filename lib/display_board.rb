811d3af41b80036752849101e0234178057a4ae1
Milos Popovic <milos_popovic@outlook.com>
2020-05-14 21:13:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = ["   ", "|", "   ", "|", "   ", "-----------", "   ", "|", "   ", "|", "   ",  "-----------", "   ", "|", "   ", "|", "   "]
+def display_board(arr)
+  puts "#{arr[0]}#{arr[1]}#{arr[2]}#{arr[3]}#{arr[4]}"
+  puts "#{arr[5]}"
+  puts "#{arr[6]}#{arr[7]}#{arr[8]}#{arr[9]}#{arr[10]}"
+  puts "#{arr[11]}"
+  puts "#{arr[12]}#{arr[13]}#{arr[14]}#{arr[15]}#{arr[16]}"
+end
+display_board(board)
